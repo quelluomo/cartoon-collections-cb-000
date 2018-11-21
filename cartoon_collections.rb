2ef5cb13c674eead55195bc6875e8c617a51924c
Matteo Muscas <thatman.j@gmail.com>
2018-11-21 10:42:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,7 @@
 def roll_call_dwarves(dwarves)
-dwarves.each_with_index.collect { |dwarf, index|
-     puts "#{index + 1}. #{dwarf}" }
+  dwarves.each_with_index do |dwarf, index|
+     puts "#{index + 1}. #{dwarf}"
+   end
   end
 
 end
