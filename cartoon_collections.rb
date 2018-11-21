db6024d09f281fd002e520a97002a3ed55df26ea
Matteo Muscas <thatman.j@gmail.com>
2018-11-21 11:32:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
   food.find(ifnone = nil) do |cheese|
-    if cheese = cheese_types.any?
+    if cheese == cheese_types.any?
       return cheese
     end
   end
