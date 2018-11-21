6067028aae32ea0bf1a664f7626441647030546a
Matteo Muscas <thatman.j@gmail.com>
2018-11-21 10:43:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,6 @@ def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |dwarf, index|
      puts "#{index + 1}. #{dwarf}"
    end
-  end
-
 end
 
 def summon_captain_planet# code an argument here
