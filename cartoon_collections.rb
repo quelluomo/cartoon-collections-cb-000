7f47eb76bbdd64aecb035d91bb447adeb6476a42
Matteo Muscas <thatman.j@gmail.com>
2018-11-21 11:13:00 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,10 +11,8 @@ def summon_captain_planet(shouts)
 end
 
 def long_planeteer_calls(calls)
-  i = 0
-  while i < calls.length
-    return true if (calls[i] > 4)
-    i = i + 1
+  calls.any? do |call|
+    call.length > 4
   end
 end
 
